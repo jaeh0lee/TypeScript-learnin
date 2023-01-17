fa61f9624165e0f1198648c8843d7d7cc4e4641f
STRONGSHARK <woghfghf@naver.com>
2023-01-17 17:24:28 +0900

    crypto 모듈의 대해 학습 및 다양한 패키지에 대해배움

--- typeChain/src/index.ts
+++ typeChain/src/index.ts
@@ -1,1 +1,25 @@
-import { init, exit } from "./myPackage";
+// 블록체인은 말 그래도 여러개의 블록이 사슬처럼인것이다.
+
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.caculateHash(prevHash, height, data);
+  }
+  static caculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
